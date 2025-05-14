19609b7c24535e7272ddce7624f5d9b70ce1a4dc
claudiogoncalves <claudiogoncalves@handtalk.me>
2025-05-14 11:00:15 -0300

    new[lspconfig]

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,14 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
+local servers = {
+  "html",
+  "cssls",
+  "terraformls",
+  "pyright",
+  "yamlls",
+  "tflint",
+  "gopls"
+}
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
