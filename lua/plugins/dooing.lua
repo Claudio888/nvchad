d71cc4ad29c044581a5b73dd1edaa313cb3848f6
claudio <claudiogfilho8@gmail.com>
2025-09-15 21:03:49 -0300

    new[dooing]

--- lua/plugins/dooing.lua
+++ lua/plugins/dooing.lua
@@ -1,8 +1,12 @@
 return {
-    "atiladefreitas/dooing",
-    config = function()
-        require("dooing").setup({
-            -- your custom config here (optional)
-        })
-    end,
+  "atiladefreitas/dooing",
+  lazy = false,
+  config = function()
+    -- Configuração opcional do plugin (caso queira definir algo)
+    -- Por exemplo, definir o arquivo padrão de tarefas:
+    vim.g.dooing_file = "~/todo.md"
+  end,
+  cmd = { "Dooing", "DooingToday", "DooingTasks" },  -- Comandos expostos pelo plugin
+  ft = { "markdown", "text" },  -- Tipos de arquivos onde faz sentido carregar
 }
+
